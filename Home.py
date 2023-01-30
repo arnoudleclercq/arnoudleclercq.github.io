20d6a26a3ad5642aa72ae5754126b63f442ccf97
arnoudleclercq <arnoudleclercq@gmail.com>
2023-01-30 12:48:52 +0100

    fresh start

---
+++ Home.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png", width=500)
+
+with col2:
+    st.title("Arnoud Leclercq")
+    content = """
+     Hi, I am Arnoud Leclercq and this is my Python portfolio site. 
+     On this site you can find the source code of some of the apps and programs that I created via Python.
+     (add some more descriptions)
+     """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built with Python. Feel free to contact me!.
+"""
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
